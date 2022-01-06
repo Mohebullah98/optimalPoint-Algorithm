c4c3c51ad2133ea3b94dbe59af9689350a1b6369
Mohebullah Mir <56857610+Mohebullah98@users.noreply.github.com>
2022-01-06 17:44:35 -0500

    Update index.js

--- index.js
+++ index.js
@@ -11,22 +11,23 @@ function optimalPoint(magic, dist) {
         
         count=0;
         for(let j=i;count!=magic.length;j++){
-        currentMagic+=magic[j];
-        if(j+1===magic.length) next =0-magic.length;
-        else next=1;
-        currentMagic +=-dist[j];
-        console.log(currentMagic+" "+count);
-        if(currentMagic<0) break;
-        //Completed a full loop and magic has not been negative, then set the optimal point as our starting point ,i and return it.
-        if(currentMagic>0&&count===magic.length-1){
-          optimalPoint=i;
-          return optimalPoint;
-        }
+          currentMagic+=magic[j];
+          if(j+1===magic.length) next =0-magic.length;
+          else next=1;
+          currentMagic +=-dist[j];
+          //console.log(currentMagic+" "+count);
+          if(currentMagic<0) break;
+          //Completed a full loop and magic has not been negative, then set the optimal point as our starting point ,i and return it.
+          if(currentMagic>0&&count===magic.length-1){
+            optimalPoint=i;
+            return optimalPoint;
+            }
         //Want to treat the array as a circle, so when you reach the end restart at the first spot.
         if(j+1===magic.length) j =j+next;
         count++;
-    }
-    return optimalPoint;
+        }
+    
 }
+    return optimalPoint;
 }
 console.log(optimalPoint(magic,dist));
